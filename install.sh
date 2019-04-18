94cc7b0f2eca48910fcc21c208b4ce02304d52e7
andrylavr <andrylavr@gmail.com>
2019-04-18 20:25:54 +0300

    init

---
+++ install.sh
@@ -0,0 +1,6 @@
+mkdir -p /opt/dictionaries
+mkdir -p /etc/clickhouse-server
+cp geoip_dictionary.xml /etc/clickhouse-server/geoip_dictionary.xml
+cp GeoLite2-ASN-Blocks-IPv4.csv /opt/dictionaries/GeoLite2-ASN-Blocks-IPv4.csv
+cp GeoLite2-Country-Blocks-IPv4.csv /opt/dictionaries/GeoLite2-Country-Blocks-IPv4.csv
+cp GeoLite2-Country-Locations-en.csv /opt/dictionaries/GeoLite2-Country-Locations-en.csv
